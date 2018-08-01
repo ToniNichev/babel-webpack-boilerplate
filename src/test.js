ee22157d215478b2e3459c4aff7546dc527adb12
Toni Nichev <toni.nichev@nbcuni.com>
2018-08-01 17:16:12 -0400

    Addingg webpack config

--- src/test.js
+++ src/test.js
@@ -1,5 +1,8 @@
 const test = (msg) => {
     console.log(msg);
+    const arr = [1, 2, 3];
+    const iAmJavascriptES6 = () => console.log(...arr);
+  window.iAmJavascriptES6 = iAmJavascriptES6;
 }
 
 export default test;
