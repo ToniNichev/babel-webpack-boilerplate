8f581f915264e1b94dabed7d0a42a2a022d84c60
Toni Nichev <toninichev@Tonis-MacBook-Pro.local>
2018-08-02 19:50:21 -0400

    Adding eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const path = require('path');
+
 module.exports = {
   entry: [
-    './src/index.js'
+    './src/app.js'
   ],
   module: {
     rules: [
+      {
+        enforce: 'pre',
+        test: /\.js?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }, 
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -12,5 +20,11 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  output: {
+    filename: '[name].bundle-test.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    // chunkFilename: '[name].[chunkhash].js'
+  },  
 };
